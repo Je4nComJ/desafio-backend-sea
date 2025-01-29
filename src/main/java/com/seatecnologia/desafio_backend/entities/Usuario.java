d774aeb901c4f6948bdc6278c4a330e3dd30b032
Jean Victor <jeanvictor.fa@gmail.com>
2025-01-29 18:18:15 -0300

    Implementando interface UserDetails + adicionando dependencia do oauth2

--- src/main/java/com/seatecnologia/desafio_backend/entities/Usuario.java
+++ src/main/java/com/seatecnologia/desafio_backend/entities/Usuario.java
@@ -1,8 +1,14 @@
 package com.seatecnologia.desafio_backend.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,7 +31,7 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_usuario")
-public class Usuario implements Serializable{
+public class Usuario implements UserDetails, Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
@@ -43,4 +49,10 @@ public class Usuario implements Serializable{
 			joinColumns = @JoinColumn(name = "user_id"),
 			inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
 }
